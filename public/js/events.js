18105c0fa42965cd41b4797ba384cf4f84eed80a
Joe Vogel <joejvogel@gmail.com>
2018-11-21 13:44:48 -0200

    Finalizado o grafico

--- public/js/events.js
+++ public/js/events.js
@@ -22,29 +22,132 @@ $(document).ready(function () {
       }
     });
 
+    //Need to avoid propagation (call to /?)
+    return false;
 
   });
+
 
   // ---------
 
 });
 
+function formatDate(dateString) {
+  var date = new Date(dateString),
+    day = date.getDate().toString().padStart(2, '0'),
+    month = (date.getMonth() + 1).toString().padStart(2, '0'), //+1 pois no getMonth Janeiro começa com zero.
+    year = date.getFullYear(),
+    hour = date.getHours().toString().padStart(2, '0'),
+    minute = date.getMinutes().toString().padStart(2, '0');
+  return day + "/" + month + "/" + year + ' ' + hour + ':' + minute;
+}
+
 function generateCharts(data) {
 
   let humData = [];
   let tempData = [];
+  let dateData = [];
 
-  data.forEach(element => {
-    humData.push({
-      x: element.date,
-      y: element.humidity
-    });
-    tempData.push({
-      x: element.date,
-      y: element.temperature
-    });
+  for (var i in data) {
+    humData[i] = data[i].humidity;
+    tempData[i] = data[i].temperature;
+    dateData[i] = formatDate(data[i].date);
+  }
+
+  Highcharts.chart('chart-element', {
+    chart: {
+      scrollablePlotArea: {
+        minWidth: 700
+      }
+    },
+
+    title: {
+      text: 'Umidade vs Temperatura'
+    },
+
+    subtitle: {
+      text: 'Clique na legenda para ocultar / mostrar a série de dados'
+    },
+
+    xAxis: {
+      // tickInterval: 24 * 3600 * 1000, // daily
+      // tickWidth: 0,
+      alignTicks: true,
+      gridLineWidth: 1,
+      categories: dateData,
+      labels: {
+        align: 'left',
+        x: 3,
+        y: -3
+      }
+    },
+
+    yAxis: [{ // left y axis
+      title: {
+        text: 'Umidade (%)'
+      },
+      labels: {
+        format: '{value}',
+        style: {
+          fontSize: '10px'
+        }
+      }
+      // showFirstLabel: false
+    }, { // right y axis
+      title: {
+        text: "Temperatura (°C)"
+      },
+      labels: {
+        format: '{value}',
+        style: {
+          fontSize: '10px'
+        }
+      },
+      opposite: true,
+      gridLineWidth: 0
+      // showFirstLabel: false
+    }],
+
+    legend: {
+      align: 'left',
+      verticalAlign: 'top',
+      borderWidth: 0
+    },
+
+    tooltip: {
+      shared: true,
+      crosshairs: true
+    },
+
+    plotOptions: {
+      series: {
+        cursor: 'pointer',
+        marker: {
+          lineWidth: 1
+        }
+      }
+    },
+
+    series: [{
+      name: 'Umidade',
+      lineWidth: 4,
+      marker: {
+        radius: 4
+      },
+      data: humData,
+      tooltip: {
+        valueSuffix: ' %'
+      }
+    }, {
+      name: 'Temperatura',
+      data: tempData,
+      color: '#ff5959',
+      yAxis: 1,
+      tooltip: {
+        valueSuffix: ' °C'
+      }
+    }]
   });
-
 
   $("#chart-area").show();
 }
